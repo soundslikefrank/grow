dcca48f342e39a379fdbb42b20d90ddb7c4d9555
Christian Maniewski <code@chmanie.com>
2020-09-08 11:59:20 +0200

    Create compile_commands.json

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@ int main() {
 
   HUART1_Init();
 
-  char msg[20] = "Hello waldo!";
+  char msg[20] = "Hello otter!";
 
   while (true) {
     HAL_UART_Transmit(&huart1, reinterpret_cast<uint8_t*>(msg), strlen(msg),
